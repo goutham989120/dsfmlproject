63358402039364f2ca8395b5e024a272e53d200f
gouthamsm <58075175+gouthamsm@users.noreply.github.com>
2025-10-14 11:26:33 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -15,3 +15,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+# if __name__ == "__main__":
+#     # logger = logging.getLogger(__name__)
+#     logging.info("Logging has started")
+#     logging.info("Logging has ended")
